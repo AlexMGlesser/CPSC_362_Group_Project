dbcdb8c54cc1d2de60d6a9c14de27631946f2854
Alex Glesser <alexglesser@csu.fullerton.edu>
2025-09-07 20:41:04 -0700

    updated todo

--- Game_List.cpp
+++ Game_List.cpp
@@ -5,7 +5,7 @@
 
 /*=========================================
 Class: Game_List
-
+TODO: Implement constructor. Also put in a map and implement an interator for searching through the whole map
 =========================================*/
 
 Game_List::Game_List() {
