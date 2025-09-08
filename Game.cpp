e4df6e02e82a8665f19a9d4d0f8eb239f557ad16
Alex Glesser <alexglesser@csu.fullerton.edu>
2025-09-07 19:45:39 -0700

    Added constructor to Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -11,7 +11,18 @@ Class: Game
 
 =========================================*/
 
-Game::Game() {
 
+Game::Game(std::string cname,
+           std::string cdescription,
+           std::vector<std::string> cgenres,
+           std::pair<std::string, bool> cachievements,
+           bool ccompletionStatus,
+           bool cfinished_achievements,
+           float chours_played) : name(std::move(cname)),
+        game_description(std::move(cdescription)),
+        genres(std::move(cgenres)),
+        achievements(std::move(cachievements)),
+        completionStatus(ccompletionStatus),
+        finished_achievemnts(cfinished_achievements),
+        hours_played(chours_played) {}
 
-}
