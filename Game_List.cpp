2b1799262a5a82f2d268cc08c09fdf656e755847
Alex Glesser <alexglesser@csu.fullerton.edu>
2025-09-09 16:35:57 -0700

    Removed dumb comments (I wrote them)

--- Game_List.cpp
+++ Game_List.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 #include "Game_List.h"
 
-
-
-/*=========================================
-Class: Game_List
-TODO: Implement constructor. Also put in a map and implement an interator for searching through the whole map
-=========================================*/
-
 Game_List::Game_List() {
 
 
--- Game_List.hpp
+++ Game_List.hpp
@@ -1,11 +1,6 @@
 #ifndef GAMELIST_H
 #define GAMELIST_H
 
-/*=========================================
-Class: Game_List
-
-=========================================*/
-
 class Game_List {
     private:
 
