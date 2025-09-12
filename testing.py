465749e48e5dc964433b23d678837b5ccab5d430
Eli Thomas <38369558+eliThomass@users.noreply.github.com>
2025-09-11 17:10:05 -0700

    pusing .py file again, didn't work last time? have a few Steam API calls set up

---
+++ testing.py
@@ -0,0 +1,32 @@
+import json, requests
+
+BASE = "https://api.steampowered.com"
+API_KEY = "7A1CB2369762358123995B31703363C5"
+
+
+def get_owned_games(steamid: int):
+    url = f"{BASE}//IPlayerService/GetOwnedGames/v0001/"
+    payload = {"steamid" : steamid}
+    params = {
+        "key": API_KEY,
+        "format": "json",
+        "input_json": json.dumps(payload)
+    }
+    return requests.get(url, params=params).json()
+
+def get_recently_played_games(steamid: int, count: int = 5):
+    url = f"{BASE}//IPlayerService/GetRecentlyPlayedGames/v0001/"
+    payload = {"steamid" : steamid}
+    params = {
+        "key": API_KEY,
+        "format": "json",
+        "input_json": json.dumps(payload)
+    }
+    return requests.get(url, params=params).json()
+
+if __name__ == "__main__":
+    print(get_recently_played_games(76561198258366170))
+
+
+    
+
