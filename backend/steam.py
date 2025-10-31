565f5f4a6bfa13175239294d1839b41a3b398cc3
Alex Glesser <alexglesser@csu.fullerton.edu>
2025-10-30 18:28:25 -0700

    updated steam.py

--- backend/steam.py
+++ backend/steam.py
@@ -27,6 +27,36 @@ def get_recently_played_games(steamid: int, count: int = 5):
     }
     return requests.get(url, params=params).json()
 
+def get_game_achievements(steamid: int, appid: int):
+    url = f"{BASE}/ISteamUserStats/GetPlayerAchievements/v0001/"
+    params = {
+        "key": API_KEY,
+        "format": "json",
+        "steamid": steamid, 
+        "appid": appid
+    }
+    return requests.get(url, params=params).json()
+
+def parse_rp_json(input_json):
+    response = input_json.get("response", {})
+    games = response.get("games", [])
+
+    for game in games:
+        print(f"{game['appid']}: {game['name']} - {game['playtime_forever']} minutes")
+
+def parse_og_json(input_json):
+    response = input_json.get("response", {})
+    games = response.get("games", [])
+
+    for game in games:
+        print(f"{game['appid']}: {game['playtime_forever']} minutes")
+
+print("Recently played games: ")
+parse_rp_json(get_recently_played_games(76561198258366170))
+
+print("\nOwned Games: ")
+parse_og_json(get_owned_games(76561198258366170))
+
 
     
 
